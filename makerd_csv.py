27ad2d794ddc8b2bdd3f4789d6eb1dae03dec0d9
Paul Spurgeon <paulspur@buffalo.edu>
2015-07-14 14:56:51 -0400

    new scripts

---
+++ makerd_csv.py
@@ -0,0 +1,15 @@
+# Create .csv of sample avg read depths, given .out files created
+# by avg_readdepth.py
+
+from __future__ import print_function
+import glob
+import re
+import subprocess
+
+fout = open('./read_depths.csv','w')
+fout.write('SampleID,Read_Depth\n')
+for file in glob.glob('*.out'):
+	fin = open(file)
+	lines = fin.readlines()
+	rd = re.search('(\d+)\n$',lines[115])
+	fout.write('{0}_AAA,{1}.0\n'.format(file[0:4],rd.group(1)))
