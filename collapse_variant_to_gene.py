30141d8d76d249edd3251de756eb34774b22e309
Paul Spurgeon <paulspur@buffalo.edu>
2015-11-03 14:30:01 -0500

    updated: new reqs

--- collapse_variant_to_gene.py
+++ collapse_variant_to_gene.py
@@ -1,4 +1,7 @@
-# This script takes in 2 command-line arguments, the names of the input and output files.
+# This script takes in 2 command-line arguments, the names of the input and 
+# output files.
+# Output is 2 separate files, one with .gt appended and one with .sev appended 
+# to provided output name.
 
 from __future__ import print_function
 import sys
@@ -6,29 +9,46 @@ import re
 
 # Taken from http://annovar.openbioinformatics.org/en/latest/user-guide/gene/
 # with 'synonymous' replaced by '.' to account for presence of 'splicing' Func
-severity = {'frameshift insertion':1,
-            'frameshift deletion':2,
-            'frameshift block substition':3,
-            'stopgain':4,
-            'stoploss':5,
+# and removal of any synonymous variant
+severity = {'frameshift insertion':11,
+            'frameshift deletion':10,
+            'frameshift block substition':9,
+            'stopgain':8,
+            'stoploss':7,
             'nonframeshift insertion':6,
-            'nonframeshift deletion':7,
-            'nonframeshift block substitution':8,
-            'nonsynonymous SNV':9,
-            '.':10,
-            'unknown':11}
+            'nonframeshift deletion':5,
+            'nonframeshift block substitution':4,
+            'nonsynonymous SNV':3,
+            '.':2,
+            'unknown':1}
 
-# Taken from rush:/projects/big/SIM/WGS/vcf/SIM_snp_recalibrated_indel_recalibrated.vcf
-samples = ('1344','1394','1406','1409','2003','2017','2021','2023','2024','2042',
-          '2048','2052','2059','2063','2066','2072','2074','2077','2078','2087',
-          '2102','2134','2142','2145','2152','2155','2157','2161','2162','2166',
-          '2168','2170','2171','2173','2179','2181','2184','2185','2193','2194',
-          '2196','2198','2208','2214','2221','2239','2257','2261','2269','2517')
-
-def collapse(f):
-  samdict = {}
+# Find header line which lists sample names. Parse them out and store them in list. 
+def getSampleIDs(f):
+  discards = ('#CHROM','POS','ID','REF','ALT','QUAL','FILTER','INFO','FORMAT')
   with open(f,'r') as fin:
     for line in fin:
+      line = line.strip()
+      if line.startswith('#CHROM'):
+        sams = line.split('\t')
+        sams = [item for item in sams if item not in discards]
+        break
+  return(sams)
+
+# General flow: for each line in file: split by tabs, process only lines with
+# desired attributes. For each one of those, convert ExonicFunc.refGene (severity)
+# field to numerical value and for each gene in the Gene.refGene field add the
+# sample:severity to the gene's severity dict, or update it if present and current # severity > stored severity.
+# At the same time, perform the same process on each line for the genotype dict,
+# updating it only when currenty severity >= stored severity and gt count > stored
+# count. Any samples not present for a given gene receive placeholder severity and
+# gt count of 0 to help with properly formatting output.
+def collapse_to_matrix(f,num):
+  nt = ['A','C','T','G']
+  sevdict = {}
+  gtdict = {}
+  with open(f,'r') as fin:
+    for line in fin:
+      line = line.strip()
       cols = line.split('\t')
       Func_refGene = cols[5]
       Gene_refGene = cols[6]
@@ -37,7 +57,7 @@ def collapse(f):
       Alt = cols[4]
       if ((Func_refGene.startswith('exonic')) or (Func_refGene.startswith('splicing'))) and \
           (not ExonicFunc_refGene.startswith('synonymous')) and \
-          (len(Ref) == 1) and (len(Alt) == 1): # only SNPs
+          (Ref in nt) and (Alt in nt): # only SNPs
         sev = severity[cols[8]]
         if (',' in Gene_refGene): # if multiple genes in field
           genes = Gene_refGene.split(',')
@@ -45,31 +65,62 @@ def collapse(f):
           genes = []
           genes.append(Gene_refGene)
         for gene in genes:
-          samdict.setdefault(gene,{})
-          for i,sam in enumerate(cols[-50:]): # the 50 genotype fields  
-            if (('0/0' in sam) or ('./.' in sam)):
-              samdict[gene].setdefault(samples[i],99)
-            elif (samples[i] in samdict[gene]) and (sev < samdict[gene][samples[i]]):
-              samdict[gene][samples[i]] = sev
+          sevdict.setdefault(gene,{})
+          gtdict.setdefault(gene,{})
+          for i,sam in enumerate(cols[-num:]): # the genotype fields  
+            gt = sam.split(':')[0].split('/')
+            gtsum = 0
+            if '.' in gt:
+              gtsum = 0
             else:
-              samdict[gene].setdefault(samples[i],sev) 
-  return(samdict)
+              gtsum = int(gt[0]) + int(gt[1])
+            if (('0/0' in sam) or ('./.' in sam)):
+              sevdict[gene].setdefault(samples[i],0)
+              gtdict[gene].setdefault(samples[i],0)
+            elif (samples[i] in sevdict[gene]) and (sev >= sevdict[gene][samples[i]]):
+              sevdict[gene][samples[i]] = sev
+              if (gtsum > gtdict[gene][samples[i]]): 
+                gtdict[gene][samples[i]] = gtsum
+            else:
+              sevdict[gene].setdefault(samples[i],sev)
+       	      gtdict[gene].setdefault(samples[i],gtsum)
+  return(sevdict,gtdict)
 
+def print_severity_matrix(sam,sev_matrix,outfile):
+  with open(outfile+'.sev','w') as fout:
+    fout.write('\t')
+    for s in sam:
+      fout.write('{}\t'.format(s))
+    fout.write('\n')
+    for gene,value in sev_matrix.iteritems():
+      fout.write('{}\t'.format(gene))
+      for k,v in sorted(value.iteritems()):
+        if v == 0:
+          fout.write('0\t')
+        else:
+          fout.write('{}\t'.format(v))
+      fout.write('\n')
+
+def print_gt_matrix(sam,gt_matrix,outfile):
+  with open(outfile+'.gt','w') as fout:
+    fout.write('\t')
+    for s in sam:
+      fout.write('{}\t'.format(s))
+    fout.write('\n')
+    for gene,value in gt_matrix.iteritems():
+      fout.write('{}\t'.format(gene))
+      for k,v in sorted(value.iteritems()):
+        if v == 99:
+          fout.write('0\t')
+        else:
+          fout.write('{}\t'.format(v))
+      fout.write('\n')
 
 if __name__ == '__main__':
   filein = sys.argv[1]
   fileout = sys.argv[2]
-  samdict = collapse(filein)
-  with open(fileout,'w') as fout:
-    fout.write('\t')
-    for s in samples:
-      fout.write('{}\t'.format(s))
-    fout.write('\n')
-    for gene,value in samdict.iteritems():
-      fout.write('{}\t'.format(gene))
-      for k,v in sorted(value.iteritems()):
-        if v == 99:
-          fout.write('\t')
-        else:
-          fout.write('{}\t'.format(v))
-      fout.write('\n')
+  samples = getSampleIDs(filein)
+  number_of_samples = len(samples)
+  sev_dict,gt_dict = collapse_to_matrix(filein,number_of_samples)
+  print_severity_matrix(samples,sev_dict,fileout)
+  print_gt_matrix(samples,gt_dict,fileout)
